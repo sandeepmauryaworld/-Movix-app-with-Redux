02fa13a85f80675f98a36b3ddfc8cd38abf71c0a
Sandeepmauryaworld <85479357+sandeepmauryaworld@users.noreply.github.com>
2023-08-16 15:24:33 +0530

    TMDB API setup - API Testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api";
 function App() {
+  useEffect(() => {
+    ApiTesting();
+  }, []);
+  const ApiTesting = () => {
+    fetchDataFromApi(`/movie/popular`).then((res) => {
+      console.log(res);
+    });
+  };
   return (
     <>
       <div className="App">App</div>
---
+++ src/utils/api.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+
+const TMDB_TOKEN =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOTFmZDdkMGJkM2RhYzI3MjU0ZWRlNTcxMTE0ZmIwOSIsInN1YiI6IjYwZTkyZTMxYTNkMDI3MDA0Njc3NjQ2MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YAuYGX2mMm8KcjnW2H2N2YfDrOBolO5wHtJ3IcsbyMI";
+
+const headers = {
+  Authorization: "bearer " + TMDB_TOKEN,
+};
+
+export const fetchDataFromApi = async (url, params) => {
+  try {
+    const { data } = await axios.get(BASE_URL + url, {
+      headers,
+      params,
+    });
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
