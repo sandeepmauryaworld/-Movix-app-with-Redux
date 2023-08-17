bd8856c94c0179da4f730ff004a1dcd2b94554ab
Sandeepmauryaworld <85479357+sandeepmauryaworld@users.noreply.github.com>
2023-08-17 11:42:19 +0530

    homepage-hero banner section

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,21 @@ function App() {
   const dispatch = useDispatch();
   const { url } = useSelector((state) => state.home);
   console.log(url);
+
   useEffect(() => {
-    ApiTesting();
+    fetchApiConfig();
   }, []);
-  const ApiTesting = () => {
-    fetchDataFromApi("/movie/popular").then((res) => {
-      dispatch(getApiConfiguration(res));
+
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
+      console.log(res);
+
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
+      dispatch(getApiConfiguration(url));
     });
   };
   return (
--- src/pages/Home/heroBanner/HeroBanner.jsx
+++ src/pages/Home/heroBanner/HeroBanner.jsx
@@ -1,20 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.scss";
+import { useNavigate } from "react-router-dom";
+import useFetch from "../../../hooks/useFetch";
+import { useSelector } from "react-redux";
+
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import Img from "../../../components/LazyLoadImage/img";
 const HeroBanner = () => {
+  // for background img chnge on every refresh  for hero banner
+  const [background, setBackground] = useState("");
+  // jb search click pr click krnege to ye navigate krega exact match pr
+
+  const navigate = useNavigate();
+  const [query, setQuery] = useState("");
+  const { url } = useSelector((state) => state.home);
+
+  // api call kraynege- hero banner pr new img aayegi each time
+  const { data, loading } = useFetch("/movie/upcoming");
+
+  // 20 obj data mese img ki url dyanamic chnge ho
+  useEffect(() => {
+    const bg =
+      url.backdrop +
+      data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+    setBackground(bg);
+  }, [data]);
+
+  // search input type kre to state me save hoga
+  const searchQueryHandler = (e) => {
+    // chek search bar empty or not  and enter hit krne pr api call hoga bs
+
+    if (e.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
   return (
     <>
       <div className="heroBanner">
-        <div className="wrapper">
-          <div className="heroBannerContent">
-            <span className="title">Welcome to the movie app</span>
-            <span className="subTitle">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
-              eligendi delectus iure. Eum asperiores officiis temporibus, sit
-              repellendus consectetur libero alias reiciendis repellat minima
-              nam, minus maxime a voluptatibus. Voluptates, vero omnis?{" "}
-            </span>
+        {!loading && (
+          <div className="backdrop_img">
+            <Img src={background} />
           </div>
-        </div>
+        )}
+        <ContentWrapper>
+          <div className="heroBannerContent">
+            <span className="title">Welcome .</span>
+            <span className="subTitle">
+              Millions of movies, TV shows and people to discover. Explore now.
+            </span>
+
+            <div className="searchInput">
+              <input
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyUp={searchQueryHandler}
+                type="text"
+                placeholder="Search For a Movie or Tv Show..."
+              />
+
+              <button>Search</button>
+            </div>
+          </div>
+        </ContentWrapper>
       </div>
     </>
   );
